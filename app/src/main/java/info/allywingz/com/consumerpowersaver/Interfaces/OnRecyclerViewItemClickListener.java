2a5bda5af1acd034942e53846fcc856fd34b2d5e
Kiran <kirankumarwinc9@gmail.com>
2018-08-03 22:08:13 +0530

    First Update after release

--- app/src/main/java/info/allywingz/com/consumerpowersaver/Interfaces/OnRecyclerViewItemClickListener.java
+++
@@ -1,6 +0,0 @@
-package info.allywingz.com.consumerpowersaver.Interfaces;
-
-public interface OnRecyclerViewItemClickListener
-{
-    public void onRecyclerViewItemClicked(String name, String maker, String watts, String time);
-}
--- app/src/main/java/info/allywingz/com/consumerpowersaver/Storage/DrawableClickListener.java
+++
@@ -1,6 +0,0 @@
-package info.allywingz.com.consumerpowersaver.Storage;
-
-public abstract class DrawableClickListener {
-    public static enum DrawablePosition { TOP, BOTTOM, LEFT, RIGHT };
-    public abstract void onClick(DrawablePosition target);
-}
